567899c1233ac7c164f5ec43d058bafb7082255d
majinge <m1j2g3@163.com>
2016-09-05 10:05:14 +0800

    01

---
+++ rotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class rotation : MonoBehaviour
+{
+
+    public GameObject B;
+
+    float speed = 5.0f;
+    Vector3[] direction = new Vector3[3] { Vector3.up, Vector3.forward, Vector3.right };
+    void Start()
+    {
+
+    }
+    void Update()
+    {
+        Vector3 temp = direction[Random.Range(0, 2)];
+        transform.RotateAround(B.transform.position, temp, speed * Time.deltaTime);
+    }
+}
